415b1254072927cb11526d1ebdead226e57b584c
jingege <kanghj793@qq.com>
2021-06-02 22:06:02 +0800

    添加到git

---
+++ git-test-project/src/com/jingege/git/Demo.java
@@ -0,0 +1,12 @@
+package com.jingege.git;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+}
